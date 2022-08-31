42b5a6a89294341940a18ea11a37a318a18449a1
mckgar <kmgarner23@outlook.com>
2022-08-31 16:06:05 -0600

    Made model for posts

---
+++ models/post.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+      maxLength: 300
+    },
+    content: {
+      type: String,
+      maxLength: 10000
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    score: {
+      type: Number,
+      default: 0
+    },
+    comments: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }],
+    user_post: {
+      type: Boolean,
+      require: true
+    },
+    subreddit: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subreddit'
+    },
+    date_posted: {
+      type: Date,
+      default: Date.now
+    },
+    date_edited: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
